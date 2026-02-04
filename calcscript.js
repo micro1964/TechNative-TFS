28097852d5b84c3fd23cdbbcb59a0e341b99061d
Ramza <ramza_tariq@outlook.com>
2026-02-04 16:39:59 +0000

    first commit of project files

---
+++ calcscript.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(value) { 
+    display.value += value; 
+}
+
+function clearDisplay() { 
+    display.value = ""; 
+}
+
+function backspace() { 
+    display.value = display.value.slice(0, -1); 
+}
+
+function calculate() { 
+    try {
+        if (display.value.trim() === "") return;
+        display.value = eval(display.value); } 
+        catch (error) { 
+        display.value = "Error"; } 
+}
